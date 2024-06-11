9a306485b690dd1f959f113239d67f9101103fb5
mrRiten <vitalik.chu@mail.ru>
2024-06-11 19:45:45 +0300

    Add Frontend

--- MuseumSystem.Web/Controllers/PostersController.cs
+++ MuseumSystem.Web/Controllers/PostersController.cs
@@ -4,39 +4,41 @@ using MuseumSystem.Core.ModelsDTO;
 
 namespace MuseumSystem.Web.Controllers
 {
-    public class PostersController(IEventService eventService, IMuseumService museumService) : Controller
+    public class PostersController() : Controller
     {
-        private readonly IEventService _eventService = eventService;
-        private readonly IMuseumService _museumService = museumService;
+        //private readonly IEventService _eventService = eventService;
+        //private readonly IMuseumService _museumService = museumService;
 
-        [HttpGet("posters/")]
-        public async Task<IActionResult> AllPosters(string museumSlug)
-        {
-            var museum = await _museumService.GetAsync(museumSlug);
-            var events = await _eventService.GetEventByMuseum(museum.IdMuseum);
+        //[HttpGet("posters/")]
+        //public async Task<IActionResult> AllPosters(string museumSlug)
+        //{
+        //    var museum = await _museumService.GetAsync(museumSlug);
+        //    var events = await _eventService.GetEventByMuseum(museum.IdMuseum);
 
-            var postersDTO = new MuseumDTO
-            {
-                Museum = museum,
-                Events = events
-            };
+        //    var postersDTO = new MuseumDTO
+        //    {
+        //        Museum = museum,
+        //        Events = events
+        //    };
 
-            return View(postersDTO);
-        }
+        //    return View(postersDTO);
+        //}
 
         [HttpGet("posters/{slugEvent}")]
         public async Task<IActionResult> Poster(string slugEvent)
         {
-            var currentEvent = await _eventService.GetEventBySlug(slugEvent);
-            var museum = await _museumService.GetAsync(currentEvent.MuseumId);
+            //var currentEvent = await _eventService.GetEventBySlug(slugEvent);
+            //var museum = await _museumService.GetAsync(currentEvent.MuseumId);
 
-            var posterDTO = new PosterDTO
-            {
-                CurrentEvent = currentEvent,
-                Museum = museum,
-            };
+            //var posterDTO = new PosterDTO
+            //{
+            //    CurrentEvent = currentEvent,
+            //    Museum = museum,
+            //};
 
-            return View(posterDTO);
+            //return View(posterDTO);
+
+            return View();
         }
 
 
--- MuseumSystem.Web/wwwroot/js/site.js
+++ MuseumSystem.Web/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var modal = document.getElementById("modal");
+var modal_date = document.getElementById("modal_date");
+var modal_time = document.getElementById("modal_time");
+var close_btn = document.getElementById("closebtn");
+var record_item = document.getElementsByClassName("record__item");
 
-// Write your JavaScript code.
+
+for (let i = 0; i < record_item.length; i++) {
+    const time_btn_collection = record_item[i].children[1].children
+    for (let j = 0; j < time_btn_collection.length; j++) {
+        time_btn_collection[j].onclick = function () {
+            modal.style.display = "flex";
+            document.body.style.overflow = "hidden";
+
+            modal_time.innerHTML = time_btn_collection[j].innerHTML;
+
+            const target_date = record_item[i].children[0];
+            modal_date.innerHTML = target_date.innerHTML;
+        }
+    }
+}
+
+close_btn.onclick = function () {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto";
+    }
+}
